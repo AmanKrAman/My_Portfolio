7031cccad9110739757aa4fd9f6c45ac3354a133
aman kumar <aman.k@ninestars.in>
2025-02-15 23:34:33 +0530

    v0.0.7 resume download integrated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_img.svg";
+import AmanResume from "../../assets/AmanResume.pdf";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 const Hero = () => {
@@ -13,12 +14,14 @@ const Hero = () => {
         software development, machine learning, LLD, HLD, and web applications.
         Proficient in C++, Python, JavaScript, with a focus on ML algorithms.
         Experienced in building and deploying scalable applications. Proven in
-        process optimization and performance enhancement. Top 7.7% on LeetCode,
+        process optimization and performance enhancement. Top 6.5% on LeetCode,
         highlighting strong problem-solving and algorithmic skills.
       </p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My Resume</div>
+        <a href={AmanResume} download="Aman_Kumar_Resume.pdf" className="hero-resume">
+          My Resume
+        </a>
       </div>
     </div>
   );
